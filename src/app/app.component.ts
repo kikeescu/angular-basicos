1ca39a5df25db6056f4ebee28c6c1957a5489119
kikeescu <eescuf@gmail.com>
2021-11-12 15:59:34 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: ` <h1>HOLA MUNDO!!</h1> `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prev';
+  
+
 }
